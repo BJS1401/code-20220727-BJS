1d8b63cf3a7c6b983c09efbefd5bc9a296bbd75e
bhavyasethi2212 <110091597+bhavyasethi2212@users.noreply.github.com>
2022-07-27 14:01:38 +0530

    Add files via upload

---
+++ BMI_Calculator.py
@@ -0,0 +1,47 @@
+import pandas as pd
+table = pd.read_json('BMI.json')
+
+# Calculation of BMI
+
+def BMI_Calculate(x,y) :
+    WeightKg=x
+    HeightCm=y
+    BMI = WeightKg / ((HeightCm/100) ** 2)
+    return BMI
+
+# Comparison of calculated BMI against Category and associated Health Risk
+
+def Compare(x):
+    BMI = x
+    if BMI <= 18.4:
+        BMI_Category = 'Underweight'
+        Health_Risk = 'Malnutrition Risk'
+    elif (BMI > 18.4) and (BMI <= 24.9):
+        BMI_Category = 'Normal Weight'
+        Health_Risk = 'Low Risk'
+    elif (BMI > 24.9) and (BMI <= 29.9):
+        BMI_Category = 'Overweight'
+        Health_Risk = 'Enhanced Risk'
+    elif (BMI > 29.9) and (BMI <= 34.9):
+        BMI_Category = 'Moderately Obese'
+        Health_Risk = 'Medium Risk'
+    elif (BMI > 35) and (BMI <= 39.9):
+        BMI_Category = 'Severely Obese'
+        Health_Risk = 'High Risk'
+    elif BMI > 39.9:
+        BMI_Category = 'Very Severely Obese'
+        Health_Risk = 'Very High Risk'
+    return BMI_Category, Health_Risk
+
+# Adding new columns to table
+
+table['BMI'] = BMI_Calculate(table['WeightKg'], table['HeightCm'])
+table['BMI_Cat'] = table['BMI'].apply(Compare)
+table[['BMI_Category', 'Health_Risk']] = pd.DataFrame(table['BMI_Cat'].tolist(), index=table.index)
+table.drop('BMI_Cat', axis=1, inplace=True)
+print(table)
+
+# Calculation of Overweight
+
+n_overweight = len(table[table['BMI_Category'] == 'Overweight'])
+print ("Number of overweight prople","=", n_overweight );
